7756291fec5f5438c331d7d9ede202a4de12534d
tantrungse <tantrungse@gmail.com>
2025-04-26 14:15:45 +0700

    Add User model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.default_url_options = { host: 'https://simple-digital-assets-platform-a4c81d37562d.herokuapp.com', port: 3000 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,40 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+puts "Seeding users..."
+
+User.create!(
+  email: "admin@example.com",
+  password: "admin@",
+  password_confirmation: "admin@",
+  role: :admin
+)
+
+User.create!(
+  email: "customer1@example.com",
+  password: "customer1@",
+  password_confirmation: "customer1@",
+  role: :customer
+)
+
+User.create!(
+  email: "customer2@example.com",
+  password: "customer2@",
+  password_confirmation: "customer2@",
+  role: :customer
+)
+
+User.create!(
+  email: "creator1@example.com",
+  password: "creator1@",
+  password_confirmation: "creator1@",
+  role: :creator
+)
+
+User.create!(
+  email: "creator2@example.com",
+  password: "creator2@",
+  password_confirmation: "creator2@",
+  role: :creator
+)
+
+puts "Seeding done!"
